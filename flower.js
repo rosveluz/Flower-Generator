6d31240caf429373f5762d7d79afcd6bd1ff959a
rosveluz <112733712+rosveluz@users.noreply.github.com>
2023-10-25 22:18:50 +0800

    Working color and petal flower

--- flower.js
+++ flower.js
@@ -1,131 +1,123 @@
-let ctx; // Canvas 2D context
 const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
 
-function initCanvas2D() {
-    ctx = canvas.getContext('2d');
-    animate();
-}
+let previousExpression = null;
 
-function animate() {
-    requestAnimationFrame(animate);
-}
+canvas.width = canvas.width;
+canvas.height = canvas.height;
 
-document.addEventListener('DOMContentLoaded', function() {
-    const canvas = document.getElementById('myCanvas');
-    const video = document.getElementById('video'); 
+let avgColor;
+let animationStarted = false;
 
-    initCanvas2D();
+let number = 0;
+let angle = 0;
 
-    video.addEventListener('playing', async function() {
-        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
-
-        if (detections.length > 0) {
-            let expressions = detections[0].expressions;
-            let maxExpression = Object.keys(expressions).reduce((a, b) => expressions[a] > expressions[b] ? a : b);
-
-            let petalsCount;
-            switch (maxExpression) {
-                case 'sad':
-                case 'angry':
-                case 'disgusted':
-                    petalsCount = 4;
-                    break;
-                case 'neutral':
-                    petalsCount = Math.floor(Math.random() * 3) + 6;  // Random value between 6 and 8
-                    break;
-                case 'happy':
-                case 'surprised':
-                    petalsCount = Math.floor(Math.random() * 17) + 8;  // Random value between 8 and 24
-                    break;
-            }
-
-            drawFlower(petalsCount, maxExpression);
-        }
-
-        setTimeout(arguments.callee, 2000);
-    });
-});
-
-
-function drawScene() {
-    // Clear the canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
-
-function drawFlower(petalsCount, expression) {
-    drawScene();
-    const centerX = canvas.width / 2;
-    const centerY = canvas.height / 2;
-    const petalLength = 180;
-
+function getPetalCount(expression) {
+    console.log("Expression received:", expression);
     switch (expression) {
-        case "happy":
-            ctx.fillStyle = "rgba(255, 255, 0, 0.4)";
-            break;
-        case "sad":
-            ctx.fillStyle = "rgba(255, 0, 255, 0.4)";
-            break;
-        case "angry":
-            ctx.fillStyle = "rgba(255, 0, 0, 0.4)";
-            break;
+        case 'sad':
+        case 'angry':
+        case 'disgusted':
+            return 4;
+        case 'neutral':
+            return 7;
+        case 'happy':
+        case 'surprised':
+            return Math.floor(Math.random() * 17) + 8;
         default:
-            ctx.fillStyle = "rgba(0, 255, 0, 0.4)";
+            return 8;
     }
-
-    for (let i = 0; i < petalsCount; i++) {
-        const angle = (i * 360 / petalsCount) * (Math.PI / 180);
-        
-        const baseWidth = petalLength * 0.3; // The width at the base of the petal
-        const tipWidth = petalLength * 0.1;  // The width at the tip of the petal
-
-        const midX = centerX + 0.5 * petalLength * Math.cos(angle);
-        const midY = centerY + 0.5 * petalLength * Math.sin(angle);
-
-        ctx.beginPath();
-
-        // Upper half of the petal
-        ctx.ellipse(midX, midY, baseWidth, tipWidth, angle, Math.PI, 2 * Math.PI, false);
-
-        // Lower half of the petal
-        ctx.ellipse(midX, midY, baseWidth, tipWidth, angle, 0, Math.PI, false);
-
-        ctx.closePath();
-        ctx.fill();
-    }
-
-    console.log("Drawing flower with", petalsCount, "petals and expression", expression);
 }
 
-function drawSingleOvalPetal() {
+function getInverseColor(color) {
+    let rgb = color.match(/\d+/g);
+    return `rgb(${255 - rgb[0]}, ${255 - rgb[1]}, ${255 - rgb[2]})`;
+}
+
+function drawPetal(ctx, canvas, avgColor, angle, maxPetals) {
+    ctx.globalCompositeOperation = 'multiply';
+
     const centerX = canvas.width / 2;
     const centerY = canvas.height / 2;
 
-    const angle = 0; // just for testing a single petal
-    const petalLength = 180;
-    
-    const baseWidth = petalLength * 0.7; // The width at the base of the petal
-    const tipWidth = petalLength * 0.1;  // The width at the tip of the petal
+    const controlDistance = canvas.width / 3;
+    const endPointDistance = canvas.width / 2 * 0.9;
 
-    const midX = centerX + 0.5 * petalLength * Math.cos(angle);
-    const midY = centerY + 0.5 * petalLength * Math.sin(angle);
+    const controlX1 = centerX + controlDistance * Math.sin(angle + Math.PI / 6);
+    const controlY1 = centerY + controlDistance * Math.cos(angle + Math.PI / 6);
+    const controlX2 = centerX + controlDistance * Math.sin(angle - Math.PI / 6);
+    const controlY2 = centerY + controlDistance * Math.cos(angle - Math.PI / 6);
+    const endX = centerX + endPointDistance * Math.sin(angle);
+    const endY = centerY + endPointDistance * Math.cos(angle);
 
-    const tipX = centerX + petalLength * Math.cos(angle);
-    const tipY = centerY + petalLength * Math.sin(angle);
+    ctx.fillStyle = avgColor;
+    ctx.strokeStyle = getInverseColor(avgColor);
+    ctx.lineWidth = 16;
 
     ctx.beginPath();
     ctx.moveTo(centerX, centerY);
-
-    // Upper half of the petal
-    ctx.ellipse(midX, midY, baseWidth, tipWidth, angle, Math.PI, 2 * Math.PI, false);
-
-    // Lower half of the petal
-    ctx.ellipse(midX, midY, baseWidth, tipWidth, angle, 0, Math.PI, false);
-
+    ctx.quadraticCurveTo(controlX1, controlY1, endX, endY);
+    ctx.quadraticCurveTo(controlX2, controlY2, centerX, centerY);
     ctx.closePath();
     ctx.fill();
+    ctx.stroke();
+
+    return angle + (2 * Math.PI) / maxPetals;
 }
 
-drawSingleOvalPetal();
+let maxPetals = 8;
+let animationDelay = 200;
+let lastTime = 0;
+
+function animate(currentTime) {
+    if (!animationStarted) {
+        return;
+    }
+
+    if (!lastTime) {
+        lastTime = currentTime;
+    }
+
+    if (currentTime - lastTime >= animationDelay) {
+        if (number < maxPetals) {
+            angle = drawPetal(ctx, canvas, avgColor, angle, maxPetals);
+            number++;
+            lastTime = currentTime;
+        } else {
+            animationStarted = false;
+            return;
+        }
+    }
+
+    requestAnimationFrame(animate);
+}
 
 
+export function startFlowerAnimation(avgColorRgb, expression) {
+    console.log("About to start flower animation with expression:", expression);
 
+    avgColor = avgColorRgb;
+
+    if (expression === previousExpression) {
+        return;
+    }
+
+    if (!animationStarted) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        number = 0;
+        maxPetals = getPetalCount(expression);
+
+        console.log("Number of petals to draw:", maxPetals); // Logging maxPetals here after determining it
+
+        let angularSpacing = (2 * Math.PI) / maxPetals;
+        let offsetAngle = (maxPetals % 2 === 1) ? angularSpacing / 2 : 0;
+
+        angle = offsetAngle;  // Start drawing from this offset
+
+        lastTime = 0;
+        animationStarted = true;
+        requestAnimationFrame(animate);
+    }
+
+    previousExpression = expression;
+}
--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+/* start of script.js */
 /* script.js */
+import { startFlowerAnimation } from './flower.js';
+
 const video = document.getElementById('video');
+
+import { getAverageColor, rgbToColorName, rgbToLab } from './utils.js';
+
+let detectedExpression = 'neutral';
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
@@ -13,162 +20,12 @@ function startVideo() {
       .then(stream => {
         video.srcObject = stream;
         video.play();
-        // Initialize the flower canvas after ensuring the video has started
-        initCanvas2D();
       })
       .catch(err => console.error(err));
 }
 
 
-function getAverageColor(src, region) {
-    const colorCanvas = document.getElementById('color-detection');
-    const context = colorCanvas.getContext('2d', { willReadFrequently: true });
-    context.drawImage(src, 0, 0, src.width, src.height);
 
-    // Ensuring the values are valid
-    region.x = Math.max(0, Math.round(region.x));
-    region.y = Math.max(0, Math.round(region.y));
-    region.width = Math.round(region.width);
-    region.height = Math.round(region.height);
-
-    try {
-        const data = context.getImageData(region.x, region.y, region.width, region.height).data;
-
-        let red = 0, green = 0, blue = 0, count = 0;
-
-        for (let i = 0; i < data.length; i += 4) {
-            red += data[i];
-            green += data[i + 1];
-            blue += data[i + 2];
-            count++;
-        }
-
-        red = Math.round(red / count);
-        green = Math.round(green / count);
-        blue = Math.round(blue / count);
-
-        return `rgb(${red}, ${green}, ${blue})`;
-    } catch (err) {
-        console.error('Error getting image data:', err);
-    }
-}
-
-function rgbToColorName(r, g, b) {
-    const colors = {
-        "White": [255, 255, 255],
-        "Black": [0, 0, 0],
-        "Red": [255, 0, 0],
-        "Green": [0, 255, 0],
-        "Blue": [0, 0, 255],
-        "Yellow": [255, 255, 0],
-        "Magenta": [255, 0, 255],
-        "Cyan": [0, 255, 255],
-        "Purple": [128, 0, 128],
-        "Pink": [255, 192, 203],
-        "Orange": [255, 165, 0],
-        "Brown": [165, 42, 42],
-        "Gray": [128, 128, 128],
-        "LightGreen": [144, 238, 144],
-        "DarkBlue": [0, 0, 139],
-        "Lavender": [230, 230, 250],
-        "Olive": [128, 128, 0],
-        "Beige": [245, 245, 220],
-        "Maroon": [128, 0, 0],
-        "Navy": [0, 0, 128],
-        
-        // "LightGray": [211, 211, 211],
-        // "Silver": [192, 192, 192],
-        // "DarkGray": [169, 169, 169],
-        // "SlateGray": [112, 128, 144],
-        // "DimGray": [105, 105, 105],
-        // "Gainsboro": [220, 220, 220],
-        // "AshGray": [178, 190, 181],
-
-        // Off-White shades
-        "Snow": [255, 250, 250],
-        "Honeydew": [240, 255, 240],
-        "MintCream": [245, 255, 250],
-        "Azure": [240, 255, 255],
-        "Ivory": [255, 255, 240],
-        "FloralWhite": [255, 250, 240],
-        "AntiqueWhite": [250, 235, 215],
-        "Linen": [250, 240, 230],
-        "LavenderBlush": [255, 240, 245],
-
-        // Additional Reds
-        "Crimson": [220, 20, 60],
-        "DarkRed": [139, 0, 0],
-        "FireBrick": [178, 34, 34],
-        "LightCoral": [240, 128, 128],
-        "IndianRed": [205, 92, 92],
-
-        // Additional Blues
-        "RoyalBlue": [65, 105, 225],
-        "MediumBlue": [0, 0, 205],
-        "SkyBlue": [135, 206, 235],
-        "SteelBlue": [70, 130, 180],
-        "PowderBlue": [176, 224, 230],
-
-        // Additional Greens
-        "ForestGreen": [34, 139, 34],
-        "DarkGreen": [0, 100, 0],
-        "SeaGreen": [46, 139, 87],
-        "LightGreen": [144, 238, 144],
-        "LimeGreen": [50, 205, 50],
-        "PaleGreen": [152, 251, 152],
-        "MediumSeaGreen": [60, 179, 113]
-    };
-
-    let closestColor = "Unknown";
-    let minDistance = Infinity;
-
-    for (const colorName in colors) {
-        const color = colors[colorName];
-        const distance = Math.sqrt((r - color[0]) ** 2 + (g - color[1]) ** 2 + (b - color[2]) ** 2);
-
-        if (distance < minDistance) {
-            minDistance = distance;
-            closestColor = colorName;
-        }
-    }
-
-    return closestColor;
-}
-
-function rgbToLab(red, green, blue) {
-    // Step 1: Convert RGB to range 0-1
-    red /= 255;
-    green /= 255;
-    blue /= 255;
-
-    // Step 2: Convert RGB to XYZ
-    var r = (red > 0.04045) ? Math.pow((red + 0.055) / 1.055, 2.4) : red / 12.92;
-    var g = (green > 0.04045) ? Math.pow((green + 0.055) / 1.055, 2.4) : green / 12.92;
-    var b = (blue > 0.04045) ? Math.pow((blue + 0.055) / 1.055, 2.4) : blue / 12.92;
-
-    var X = r * 0.4124564 + g * 0.3575761 + b * 0.1804375;
-    var Y = r * 0.2126729 + g * 0.7151522 + b * 0.0721750;
-    var Z = r * 0.0193339 + g * 0.1191920 + b * 0.9503041;
-
-    X = X * 100;
-    Y = Y * 100;
-    Z = Z * 100;
-
-    // Step 3: Convert XYZ to Lab
-    X /= 95.047;
-    Y /= 100.000;
-    Z /= 108.883;
-
-    var fx = (X > 0.008856) ? Math.pow(X, 1/3) : (903.3 * X + 16) / 116;
-    var fy = (Y > 0.008856) ? Math.pow(Y, 1/3) : (903.3 * Y + 16) / 116;
-    var fz = (Z > 0.008856) ? Math.pow(Z, 1/3) : (903.3 * Z + 16) / 116;
-
-    var L = (116 * fy) - 16;
-    var a = 500 * (fx - fy);
-    var bLab = 200 * (fy - fz);
-
-    return [L, a, bLab];
-}
 
 
 video.addEventListener('play', () => {
@@ -193,6 +50,14 @@ video.addEventListener('play', () => {
         if (resizedDetections[0]) {
         const faceBox = resizedDetections[0].detection.box;
 
+        if (resizedDetections[0] && resizedDetections[0].expressions) {
+            const emotion = Object.keys(resizedDetections[0].expressions).reduce((a, b) => 
+                resizedDetections[0].expressions[a] > resizedDetections[0].expressions[b] ? a : b
+            );
+            faceExpressionElement.innerText = emotion;
+            detectedExpression = emotion; // Store the detected expression
+        }
+
         const colorRegion = {
             x: faceBox.x / 1.5,
             y: faceBox.y + faceBox.height, // this should give you the y-coordinate of the bottom of the face box
@@ -201,6 +66,11 @@ video.addEventListener('play', () => {
         };
 
         const avgColorRgb = getAverageColor(video, colorRegion);
+        
+        if (detectedExpression !== 'neutral') {
+            startFlowerAnimation(avgColorRgb, detectedExpression); // <-- Start the flower animation
+        }
+
         const avgColorName = rgbToColorName(...avgColorRgb.split('(')[1].split(')')[0].split(',').map(val => +val.trim()));
         document.getElementById('colorDetection').innerText = `${avgColorName}`;
 
@@ -218,4 +88,5 @@ video.addEventListener('play', () => {
         
     }, 100);
 });
+/* end of script.js */
 
